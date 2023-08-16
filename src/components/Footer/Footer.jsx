717d68ab279ccfe177edde65e4938324074aaea4
annakreno <annakreno@gmail.com>
2023-08-16 10:18:53 -0500

    minor alterations to copy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 export default function Footer() {
     return (
       <div className="footer-container">
-        <div>Scott W Sound LLC - 2023</div>
+        <div>Scott W Sound - 2023</div>
         <div>Chicago, IL</div>
         <a href="https://annakreno.github.io/portfolio/" id="designer-link">website by Anna Reno</a>
         <div className='footer-logo' id="footer-logo">
